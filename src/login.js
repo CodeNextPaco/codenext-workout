71e3289954a4e7ca9adba4081ad411599810e84e
encomp <edgar.martinez.rico@gmail.com>
2020-11-06 22:24:48 -0800

    Branch 03

--- src/login.js
+++ src/login.js
@@ -1,11 +1,12 @@
 import 'bootstrap';
 import './scss/index.scss';
 import './scss/login.scss';
+import 'firebase/auth';
 import { auth } from './services/firebaseService'
 import { gotoHome } from './util/redirect'
 
 //If the user is already logged in redirect to the home page.
-auth().onAuthStateChanged(function (newuser) {
+firebase.auth().onAuthStateChanged(function (newuser) {
     if (newuser) {
         gotoHome();
     }
