eed26f72231da4c297edb45116e7b073d49b1619
encomp <edgar.martinez.rico@gmail.com>
2020-11-07 00:26:01 -0800

    Branch 03

--- src/login.js
+++ src/login.js
@@ -1,11 +1,18 @@
 import 'bootstrap';
 import './scss/index.scss';
 import './scss/login.scss';
-import 'firebase/auth';
+import firebase from 'firebase';
 import { auth } from './services/firebaseService'
 import { gotoHome } from './util/redirect'
 //TODO (8.1) Import the component.
 
-
 //TODO (8.2) Define a function that is called after the page is loaded.
 //TODO (8.3) Load the LoginButtonComponent once the page is loaded.
+
+
+//If the user is already logged in redirect to the home page.
+firebase.auth().onAuthStateChanged(function (newuser) {
+    if (newuser) {
+        gotoHome();
+    }
+});
