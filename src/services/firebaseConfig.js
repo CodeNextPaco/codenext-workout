c4e7690b6284212612cfdc317368ec351d2baee2
encomp <edgar.martinez.rico@gmail.com>
2020-11-07 09:22:08 -0800

    Branch 04

--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -1,11 +1,11 @@
 //TODO: Add your project configuration from the firebase console.
 export const firebaseConfig = {
-  apiKey: "AIzaSyB1m9Seu92cRIJK-5RPM1Tj1nonrUdrYEo",
-  authDomain: "codenext-workout.firebaseapp.com",
-  databaseURL: "https://codenext-workout.firebaseio.com",
-  projectId: "codenext-workout",
-  storageBucket: "codenext-workout.appspot.com",
-  messagingSenderId: "905691266795",
-  appId: "1:905691266795:web:b0f95bdfc411d4db5a88ae",
-  measurementId: "G-M96SNM765V"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
