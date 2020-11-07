2f58d28ae965aa3d284cf31fb879ff77510b965b
encomp <edgar.martinez.rico@gmail.com>
2020-11-07 00:53:32 -0800

    Branch 04

--- src/components/login_button/loginButton.component.js
+++ src/components/login_button/loginButton.component.js
@@ -1,2 +1,29 @@
-// TODO(7): Define the component code here.
-// TODO(14): Modify the afterRender method to handle the Authentication.
+import firebase from 'firebase';
+import 'firebase/auth';
+import { buttonLoginTemplate } from './loginButton.template';
+
+export const LoginButtonComponent = {
+
+    render(model) {
+        return buttonLoginTemplate(model);
+    },
+
+    afterRender(model, onSuccess, onError) {
+        const loginButton = document.querySelector('#' + model.id);
+        loginButton.addEventListener('click', event => {
+            const provider = model.provider;
+            console.log(model);
+            firebase.auth().signInWithPopup(provider).then(function (result) {
+                console.log(result);
+                if (onSuccess !== undefined) {
+                    onSuccess(result);
+                }
+            }).catch(function (error) {
+                console.log(error);
+                if (onError !== undefined) {
+                    onError(error);
+                }
+            });
+        });
+    },
+}
--- src/login.js
+++ src/login.js
@@ -1,18 +1,36 @@
+// Add several imports
 import 'bootstrap';
 import './scss/index.scss';
 import './scss/login.scss';
 import firebase from 'firebase';
 import { auth } from './services/firebaseService'
 import { gotoHome } from './util/redirect'
-//TODO (8.1) Import the component.
+import { LoginButtonComponent } from './components/login_button/loginButton.component';
 
-//TODO (8.2) Define a function that is called after the page is loaded.
-//TODO (8.3) Load the LoginButtonComponent once the page is loaded.
-
+// Define a function that is execute right after the page is loaded.
+window.onload = function () {
+    // Define a Firebase Provider that will be used to sign in.
+    let provider = new auth.GoogleAuthProvider();
+    provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+    const model = {
+        id: 'googleBtn',
+        image: 'fa-google',
+        provider: provider
+    };
+    // Grab from the login.html the element with id loginButtons
+    const divElement = document.getElementById("loginButtons");
+    // Add the following HTML string to the loginButtons HTML element.
+    divElement.innerHTML = LoginButtonComponent.render(model);
+    // Once the new HTML content is part of the page add the required listeners.
+    LoginButtonComponent.afterRender(model);
+};
 
 //If the user is already logged in redirect to the home page.
+// See https://firebase.google.com/docs/auth/web/manage-users
 firebase.auth().onAuthStateChanged(function (newuser) {
+    // If the user exist, meaning if the user is loged in.
     if (newuser) {
+        // Redirect to the home page
         gotoHome();
     }
 });
