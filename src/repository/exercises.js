e89007d56452cd3b114fe3f53c17201fe1d081ff
encomp <edgar.martinez.rico@gmail.com>
2020-11-07 09:26:39 -0800

    Branch 04

--- src/repository/exercises.js
+++ src/repository/exercises.js
@@ -1,13 +1,16 @@
 import { firestore } from './../services/firebaseService';
 
+// Helper function that retrieves a document reference from a specific exercise collection.
 export function getExerciseRef(exercise, userEmail, newDate) {
     return firestore.collection(exercise).doc(userEmail).collection("dates").doc(newDate);
 }
 
+// Helper function that retrieves a document from a specific exercise collection.
 export function getExercise(exercise, userEmail, newDate) {
     return firestore.collection(exercise).doc(userEmail).collection("dates").doc(newDate).get();
 }
 
+// Helper function that saves a document from a specific exercise collection.
 export function saveExercise(exercise, user, newDate, data) {
     return firestore.collection(exercise)
         .doc(user)
@@ -16,6 +19,7 @@ export function saveExercise(exercise, user, newDate, data) {
         .set(data);
 }
 
+// Helper function that updates a document from a specific exercise collection.
 export function updateExercise(docRef, data) {
     return docRef.update(data);
 }
--- src/repository/metadata.js
+++ src/repository/metadata.js
@@ -1,14 +1,17 @@
 import { Exercises, converterExercises } from './../util/exercise';
 import { firestore } from './../services/firebaseService';
 
+// Helper function that retrieves a document reference from the exercises collection.
 export function getMetaDataRef(userEmail, newDate) {
     return firestore.collection("exercises").doc(userEmail).collection("dates").doc(newDate);
 }
 
+// Helper function that retrieves a document from the exercises collection.
 export function getMetaDataDoc(userEmail, newDate) {
     return firestore.collection("exercises").doc(userEmail).collection("dates").doc(newDate).withConverter(converterExercises).get();
 }
 
+// Helper function that paginates the documents from exercises collection.
 export function getMetaDataDocuments(userEmail, limit, startAfter, endAt) {
     let query = firestore.collection("exercises").doc(userEmail).collection("dates");
     if (limit) {
@@ -23,7 +26,7 @@ export function getMetaDataDocuments(userEmail, limit, startAfter, endAt) {
     return query.withConverter(converterExercises).get();
 }
 
-
+// Helper function that save a document on the exercises collection.
 export function saveMetaData(userEmail, newDate, data) {
     return firestore.collection("exercises")
         .doc(userEmail)
@@ -33,6 +36,7 @@ export function saveMetaData(userEmail, newDate, data) {
         .set(data);
 }
 
+// Helper function that update a document on the exercises collection.
 export function updateMetaData(docRef, data) {
     return docRef
         .withConverter(converterExercises)
